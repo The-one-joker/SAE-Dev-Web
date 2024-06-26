d6798d16a9e8e4cb18d2ec26ad785a0d51621dc0
Lucas <lucas.aubriet@outlook.fr>
2024-06-26 16:31:56 +0200

    Profil 1

--- administration/profil.php
+++ administration/profil.php
@@ -7,7 +7,6 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-        <!-- Ajoutez ici les liens vers les fichiers CSS et JavaScript nécessaires -->
         <header class="p-3 mb-3 border-bottom text-bg-dark">
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -40,8 +39,10 @@
             </div>
         </header>
     </head>
-
     <body>
-        
+        <h1>Profil</h1>
+        <?php
+        include '../profil.php';
+        ?>
     </body>
 </html>
---
+++ profil.php
@@ -0,0 +1,57 @@
+<?php
+    // Configuration de la base de données
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "rythmix";
+
+    // Connexion à la base de données
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("La connexion a échoué: " . $conn->connect_error);
+    }
+
+    // Requête SQL pour sélectionner tous les utilisateurs
+    $sql = "SELECT USER_ID, Nom, Prenom, Mail, Mdp, Type FROM Utilisateurs";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Début du tableau HTML
+        echo "<table class='table' border='1'>
+                <thead>
+                    <tr>
+                        <th scope='col'>ID</th>
+                        <th scope='col'>Nom</th>
+                        <th scope='col'>Prénom</th>
+                        <th scope='col'>Mail</th>
+                        <th scope='col'>Mot de passe</th>
+                        <th scope='col'>Type</th>
+                    </tr>
+                </thead>";
+    
+        // Parcourir les résultats et ajouter des lignes au tableau
+        while($row = $result->fetch_assoc()) {
+            echo "
+                <tbody>
+                <tr>
+                    <td>" . $row["USER_ID"] . "</td>
+                    <td>" . $row["Nom"] . "</td>
+                    <td>" . $row["Prenom"] . "</td>
+                    <td>" . $row["Mail"] . "</td>
+                    <td>" . $row["Mdp"] . "</td>
+                    <td>" . $row["Type"] . "</td>
+                </tr>
+                </tbody>";
+        }
+    
+        // Fin du tableau HTML
+        echo "</table>";
+    } else {
+        echo "0 résultats";
+    }
+
+    // Fermer la connexion
+    $conn->close();
+?>
